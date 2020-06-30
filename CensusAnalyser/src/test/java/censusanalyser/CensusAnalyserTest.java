30b9ebe73bd0ce9c7be1ff27fcfdbf3794c32b78
vegirevathi <vegirevathi@gmail.com>
2020-06-30 06:43:36 +0530

    ADDED: TC 1.1 to check number of matched records

--- CensusAnalyser/src/test/java/censusanalyser/CensusAnalyserTest.java
+++ CensusAnalyser/src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -17,16 +17,4 @@ public class CensusAnalyserTest {
             Assert.assertEquals(29,numOfRecords);
         } catch (CensusAnalyserException e) { }
     }
-
-    @Test
-    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
-        try {
-            CensusAnalyser censusAnalyser = new CensusAnalyser();
-            ExpectedException exceptionRule = ExpectedException.none();
-            exceptionRule.expect(CensusAnalyserException.class);
-            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
-        } catch (CensusAnalyserException e) {
-            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
-        }
-    }
 }
