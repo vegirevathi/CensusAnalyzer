f80b07ca3f0f84ec96d304bc301828ad06bfdc10
vegirevathi <vegirevathi@gmail.com>
2020-06-30 06:39:15 +0530

    REFACTORED: Handled Exception and used java 8 stream feature

--- CensusAnalyser/src/main/java/censusanalyser/CensusAnalyser.java
+++ CensusAnalyser/src/main/java/censusanalyser/CensusAnalyser.java
@@ -13,16 +13,14 @@ import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
     public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+        CsvToBean<IndiaCensusCSV> csvToBean = null;
         try {
             Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
             CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
             csvToBeanBuilder.withType(IndiaCensusCSV.class);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
-            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();;
-            Iterable<IndiaCensusCSV> csviterable = () -> censusCSVIterator;
-            int namOfEateries = (int) StreamSupport.stream(csviterable.spliterator(), false).count();
-            return namOfEateries;
+            csvToBean = csvToBeanBuilder.build();
+
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
@@ -30,5 +28,9 @@ public class CensusAnalyser {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
         }
+        Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();;
+        Iterable<IndiaCensusCSV> csviterable = () -> censusCSVIterator;
+        int namOfEateries = (int) StreamSupport.stream(csviterable.spliterator(), false).count();
+        return namOfEateries;
     }
 }
